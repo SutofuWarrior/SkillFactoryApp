e8a85bec2f56d0fe409caa8b26c12224b35a7b7a
pdn <dima_root@mail.ru>
2020-11-20 19:23:36 +0700

    Задание 11.3.2

--- Module11/Program.cs
+++ Module11/Program.cs
@@ -6,8 +6,15 @@ namespace Module11
     {
         static void Main(string[] args)
         {
-            string MyName = "Dima";
-            Console.WriteLine(MyName);
+            string name = "Dmitry";
+            byte age = 29;
+            bool isPet = false;
+            float shoeSize = 45;
+
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"I am {age} years old");
+            Console.WriteLine($"I {(isPet ? "do have" : "don't have")} a pet");
+            Console.WriteLine($"My foot size is {shoeSize}");
 
             Console.ReadKey();
         }
