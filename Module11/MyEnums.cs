f7c85e03324af60dabd93fcd69d450d6f43e5aee
pdn <dima_root@mail.ru>
2020-11-20 20:22:06 +0700

    Final module commit

---
+++ Module11/MyEnums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module11
+{
+    public enum DaysOfWeek
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday  = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
+
+    public enum Semaphore
+    {
+        Red = 100,
+        Green = 200,
+        Yellow = 300,
+    }
+}
--- Module11/Program.cs
+++ Module11/Program.cs
@@ -6,6 +6,19 @@ namespace Module11
     {
         static void Main(string[] args)
         {
+            //Item_11_3();
+            //Item_11_7();
+            FinalTask();
+
+            //Console.WriteLine("{0}\n{1}\n{2}", name, age, favcolor);
+            //Console.WriteLine("{0}\n  {1}\n {2}", name, age, favcolor);
+            // b -= k;
+
+            Console.ReadKey();
+        }
+
+        static void Item_11_3()
+        {
             string name = "Dmitry";
             byte age = 29;
             bool isPet = false;
@@ -15,8 +28,36 @@ namespace Module11
             Console.WriteLine($"I am {age} years old");
             Console.WriteLine($"I {(isPet ? "do have" : "don't have")} a pet");
             Console.WriteLine($"My foot size is {shoeSize}");
+        }
 
-            Console.ReadKey();
+        static void Item_11_7()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and age is {age}");
+
+            Console.Write("What is your favourite day of week? ");
+            DayOfWeek day = (DayOfWeek)Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your favourite day of week is {day}");
+        }
+
+        static void FinalTask()
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your name is {name} and age is {age}");
+
+            Console.Write("Enter your birthday: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine($"Your birthday is {birthdate}");
         }
     }
 }
