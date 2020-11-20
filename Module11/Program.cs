b2f31baa92a54d234d951d0a12187e78ecdd2a0d
pdn <dima_root@mail.ru>
2020-11-20 18:59:50 +0700

    Добавьте файлы проекта.

---
+++ Module11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Dima";
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
