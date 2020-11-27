724a62093814e01cd7aa0a3f6fa6d052710ce72e
pdn <dima_root@mail.ru>
2020-11-27 17:49:09 +0700

    Модуль 12.1

--- Module11/Program.cs
+++ Module11/Program.cs
@@ -8,11 +8,9 @@ namespace Module11
         {
             //Item_11_3();
             //Item_11_7();
-            FinalTask();
+            //Item_12_FinalTask();
 
-            //Console.WriteLine("{0}\n{1}\n{2}", name, age, favcolor);
-            //Console.WriteLine("{0}\n  {1}\n {2}", name, age, favcolor);
-            // b -= k;
+            
 
             Console.ReadKey();
         }
@@ -45,7 +43,7 @@ namespace Module11
             Console.WriteLine($"Your favourite day of week is {day}");
         }
 
-        static void FinalTask()
+        static void Item_11_FinalTask()
         {
             Console.Write("Enter your name: ");
             var name = Console.ReadLine();
@@ -58,6 +56,74 @@ namespace Module11
             Console.Write("Enter your birthday: ");
             var birthdate = Console.ReadLine();
             Console.WriteLine($"Your birthday is {birthdate}");
+        }
+
+        static void Item_12_1_4()
+        {
+            string A = default;
+            string B = default;
+            bool C = A != B;
+        }
+
+        static void Item_12_1_5()
+        {
+            int A = 4;
+            int B = 10;
+            double X = 5;
+            double Y = 8;
+
+            bool C = (A < B) | (X > Y);
+        }
+
+        static void Item_12_1_10()
+        {
+            var a = 6;
+            var b = 6;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+        }
+
+        static void Item_12_1_16()
+        {
+            Console.WriteLine("Напишите свой любимый цвет консоли с маленькой буквы: ");
+
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
         }
     }
 }
